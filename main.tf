227e6e2e1e48ee8bed982d3160db4bd8d5e3787c
Nandeesh94 <nandeesh.flipkart@gmail.com>
2025-03-03 22:53:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this my application created by terraform infrastructure nandeesh server-1" >/var/www/html/index.html
+echo "hai all this my application created by terraform infrastructure nandeesh server-1" > /var/www/html/index.html
 EOF
 tags = {
 Name = "web_server-1"
@@ -33,7 +33,7 @@ sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my application created by terraform infrastructure nandeesh server-2" >/var/www/html/index.html
+echo "hai all this is my application created by terraform infrastructure nandeesh server-2" > /var/www/html/index.html
 EOF
 tags = {
 Name = "web_server-2"
